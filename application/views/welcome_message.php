14ef3a6e3d5328c37ef91a303f190caf71b14c2a
Samuel Asor <sammyskills@gmail.com>
2022-02-26 10:44:57 +0100

    Committed local files

---
+++ application/views/welcome_message.php
@@ -0,0 +1,93 @@
+<?php 
+defined('BASEPATH') OR exit('No direct script access allowed'); 
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="utf-8" />
+        <title>Login | Xoric - Responsive Bootstrap 4 Admin Dashboard</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="description" content="" />
+		
+        <!-- App favicon -->
+        <link rel="shortcut icon" href="assets/images/favicon.ico">
+
+        <!-- Bootstrap Css -->
+        <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+        <!-- Icons Css -->
+        <link href="assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+        <!-- App Css-->
+        <link href="assets/css/app.min.css" rel="stylesheet" type="text/css" />
+
+    </head>
+
+    <body class="bg-primary bg-pattern">
+        <div class="home-btn d-none d-sm-block">
+            <a href="index.html" title="Back to Main Website"><i class="mdi mdi-home-variant h2 text-white"></i></a>
+        </div>
+
+        <div class="account-pages my-5 pt-sm-5">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="text-center mb-5">
+                            <a href="index.html" class="logo"><img src="assets/images/logo-light.png" height="24" alt="logo"></a>
+                            <h5 class="font-size-16 text-white-50 mb-4">Responsive Bootstrap 4 Admin Dashboard</h5>
+                        </div>
+                    </div>
+                </div>
+                <!-- end row -->
+
+                <div class="row justify-content-center">
+                    <div class="col-xl-5 col-sm-8">
+                        <div class="card">
+                            <div class="card-body p-4">
+                                <div class="p-2">
+                                    <h5 class="mb-5 text-center">Sign in to continue to Xoric.</h5>
+                                    <form class="form-horizontal" action="index.html">
+
+                                        <div class="row">
+                                            <div class="col-md-12">
+                                                <div class="form-group form-group-custom mb-4">
+                                                    <input type="text" class="form-control" id="username" required>
+                                                    <label for="username">User Name</label>
+                                                </div>
+
+                                                <div class="form-group form-group-custom mb-4">
+                                                    <input type="password" class="form-control" id="userpassword" required>
+                                                    <label for="userpassword">Password</label>
+                                                </div>
+
+                                                <div class="mt-4">
+                                                    <button class="btn btn-primary btn-block waves-effect waves-light" type="submit">Log In</button>
+                                                </div>
+                                                <div class="mt-4 text-center">
+                                                    <a href="auth-recoverpw.html" class="text-muted"><i class="mdi mdi-lock"></i> Forgot your password?</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- end row -->
+            </div>
+        </div>
+        <!-- end Account pages -->
+
+        <!-- JAVASCRIPT -->
+        <script src="assets/libs/jquery/jquery.min.js"></script>
+        <script src="assets/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
+        <script src="assets/libs/metismenu/metisMenu.min.js"></script>
+        <script src="assets/libs/simplebar/simplebar.min.js"></script>
+        <script src="assets/libs/node-waves/waves.min.js"></script>
+
+        <!-- <script src="assets/libs/@iconscout.com/unicons/script/monochrome/bundle.js"></script> -->
+
+        <script src="assets/js/app.js"></script>
+
+    </body>
+</html>
